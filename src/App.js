f59491e60d8be5e4b298fdd2d2b2d17bb32e890f
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-05-28 23:50:01 +0500

    install react router dom and setup in app page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home/Home.jsx';
+import Login from './pages/Login/Login.jsx'
 function App() {
+ const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  }
+ ])
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <RouterProvider router={router}/>
     </div>
   );
 }
