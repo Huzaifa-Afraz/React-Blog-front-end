f49d321a3623e7ba38715434966676c05385269f
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-08-19 10:39:28 +0500

    vercel build failed erro fix: AuthContext module failed to load

--- src/context/AuthState.js
+++ src/context/AuthState.js
@@ -1,5 +1,5 @@
-import { Children, useEffect, useState } from "react";
-import AuthContext from "./AuthContext.js";
+import {useEffect, useState } from "react";
+import AuthContext  from './AuthContext.js';
 import axios from "axios";
 
 const AuthState=({children})=>{
